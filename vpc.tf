a80223cdf2a36c8bdffe0782c38daf924cf02462
SYED AYAZUDDIN <syedayazuddin@SYEDs-MacBook-Air.local>
2025-11-23 19:58:21 +0530

    VPC Module Test

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "main" {
+    source = "../Terraform-aws-VPC-Module"
+   # source = "git::https://github.com/DAWS-82S/terraform-aws-vpc.git?ref=main"
+    cidr_block = var.cidr_block
+    common_tags = var.common_tags
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
+    is_peering_required = true
+    vpc_cidr = var.vpc_cidr
+}
